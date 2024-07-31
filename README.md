e5705237fd8cc2d9d9f84fe8f785f51dda640cf6
npelcat <pelcat.nd@gmail.com>
2024-07-31 11:55:02 +0200

    first version of form with and without yup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Form without && whith Yup
+
+## React + Vite
+
+Première utilisation de la bibliothèque Yup pour la gestion des formulaires (shémas de validation)
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+//import FormWithoutYup from "./components/FormWithoutYup";
+import FormWithYup from "./components/FormWithYup";
+
+function App() {
+  return (
+    <div className="form-container">
+      <FormWithYup />
+    </div>
+  );
+}
+
+export default App;
